93da10716ffed839af18732a15e1299e3cdc1f95
Junah Park <junahp@hotmail.com>
2021-08-30 23:02:35 -0700

    saved changes

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.static('build'))
 
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
 morgan.token('data', (request) => {
     return JSON.stringify(request.body)
 })
